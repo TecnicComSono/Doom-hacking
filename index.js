754768168af47df7cc2b9dd46c855721fbbef911
JornalOrminda <117489835+TecnicComSono@users.noreply.github.com>
2025-02-02 16:19:49 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+var htmlContent = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cry now bro</title>
+    <link rel="icon" href="https://i.gifer.com/ZU4b.gif" type="image/x-icon"/>
+    <link rel="shortcut icon" href="https://i.gifer.com/ZU4b.gif" type="image/x-icon"/>
+    <meta charset="UTF-8">
+    <style>
+        body {
+            background-color: black;
+            color: white;
+            text-align: center;
+        }
+        h1, h3, h4 {
+            font-size: 46px;
+        }
+        h4 {
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+    <img src="https://i.gifer.com/ZU4b.gif" width="80" height="60"><hr>
+    <iframe src="https://emupedia.net/emupedia-game-doom2/asmjs/" width="1000" height="600" frameborder="0" allowfullscreen></iframe><hr>
+</body>
+</html>
+`;
+document.write(htmlContent);
